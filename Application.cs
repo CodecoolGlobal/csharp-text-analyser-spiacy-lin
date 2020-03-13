5a76b6cdb33bd0c68eaa415b9708b45cb6d5ce43
Cool Lin <andrzejkom2016@gmail.com>
2020-03-13 08:01:25 +0100

    aaa

--- Application.cs
+++ Application.cs
@@ -7,31 +7,8 @@ namespace csharp_text_analyser_spiacy_lin
     {
         static void Main(string[] args)
         {
-            string text = System.IO.File.ReadAllText("test.txt");
-            var wordslist = new List<string>();
-            var charslist = new List<string>();
-            int how_many_chars = 0;
-            int how_many_words = 0;
-            FileContent fcont = new FileContent(text);
-            
-            Iterator iteratorC = fcont.CharIterator();
-            while (iteratorC.HasNext())
-            {
-                string item = (string) iteratorC.MoveNext();
-                how_many_chars++;
-                charslist.Add(item);
-            }
-            Console.WriteLine(how_many_chars);
-            
-            Iterator iteratorW = fcont.WordIterator();
-            while (iteratorW.HasNext())
-            {
-                string item = (string) iteratorW.MoveNext();
-                how_many_words++;
-                wordslist.Add(item);
-            }
-            
-            Console.WriteLine(how_many_words);
+               
         }
     }
 }
+
