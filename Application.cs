638885158a9637ac04ebfadc5308783858230895
Cool Lin <andrzejkom2016@gmail.com>
2020-03-13 12:55:14 +0100

    CountOf working

--- Application.cs
+++ Application.cs
@@ -7,15 +7,17 @@ namespace csharp_text_analyser_spiacy_lin
     {
         static void Main(string[] args)
         {
-            string text = System.IO.File.ReadAllText("test.txt");
+            string text = System.IO.File.ReadAllText("mytext.txt");
             FileContent fcont = new FileContent(text);
             Iterator iteratorW = fcont.WordIterator();
             Iterator iteratorC = fcont.CharIterator();
 
             StatisticalAnalysis staW = new StatisticalAnalysis(iteratorW);
-            Console.WriteLine(staW.Size());
+            //string[] ar = {"ala","kot"};
+            Console.WriteLine(staW.CountOf("ala","kot"));            
             StatisticalAnalysis staC = new StatisticalAnalysis(iteratorC);
             Console.WriteLine(staC.Size());
+            
             
             
 
--- StatisticalAnalysis.cs
+++ StatisticalAnalysis.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace csharp_text_analyser_spiacy_lin
 {
@@ -10,15 +11,37 @@ namespace csharp_text_analyser_spiacy_lin
             this.i = i;
         }
 
-        public int Size()
+        public int CountOf(params string[] args)
+        {
+            int counter = 0;
+            foreach (string el in CreateList())
             {
-                   int how_many = 0;
-                   while (i.HasNext())
-                   {
-                       string item = (string)i.MoveNext();
-                       how_many++;
-                   }
-                   return how_many;
+                foreach (string item in args)
+                {
+                    if (el.Equals(item)) {counter++;}
+                }
             }
+            return counter; 
+        }
+        public List<string> CreateList()
+        {
+            List<string> stringList = new List<string>();
+            while (i.HasNext())
+            {
+                string item = (string)i.MoveNext();
+                stringList.Add(item);
+            }
+            return stringList;
+        }
+        public int Size()
+        {
+            int how_many = 0;
+            while (i.HasNext())
+            {
+                string item = (string)i.MoveNext();
+                how_many++;
+            }
+            return how_many;
+        }
     }
 }
