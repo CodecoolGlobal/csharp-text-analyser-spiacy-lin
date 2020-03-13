fc2e63605f3e03721260cde23f6ce3f3f3498b3b
Cool Lin <andrzejkom2016@gmail.com>
2020-03-13 10:01:49 +0100

    introduction of StatisticalAnalysis class

--- Application.cs
+++ Application.cs
@@ -7,6 +7,18 @@ namespace csharp_text_analyser_spiacy_lin
     {
         static void Main(string[] args)
         {
+            string text = System.IO.File.ReadAllText("test.txt");
+            FileContent fcont = new FileContent(text);
+            Iterator iteratorW = fcont.WordIterator();
+            Iterator iteratorC = fcont.CharIterator();
+
+            StatisticalAnalysis staW = new StatisticalAnalysis(iteratorW);
+            Console.WriteLine(staW.Size());
+            StatisticalAnalysis staC = new StatisticalAnalysis(iteratorC);
+            Console.WriteLine(staC.Size());
+            
+            
+
                
         }
     }
